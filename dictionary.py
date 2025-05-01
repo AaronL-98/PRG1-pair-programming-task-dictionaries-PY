db0b7e42d9a571d636821db23c4a033a28ecb3e8
Aaron Lewis <aaronmlewis175@gmail.com>
2025-05-01 10:07:41 +0000

    TDD operators for a library of books to match test cases

--- dictionary.py
+++ dictionary.py
@@ -39,23 +39,47 @@ def get_books_by_author(library, author_name):
 
 
 def search_by_book_name(library, search_term):
-    pass
+    book_by_name = []
+    for book in library:
+        if book["title"] == search_term:
+            book_by_name.append(book)
+            break
 
+    return book_by_name
 
 def display_loan_totals():
-    pass
+    loan_count = 0
+    not_on_loan = 0
+    
+    for book in library:
+        if book["isLoaned"]:
+            loan_count += 1  
+        else:
+            not_on_loan += 1
+
+    print(f"Total books on loan: {loan_count}")
+    print(f"Total books not on loan: {not_on_loan}")
+
 
 
 def alter_book_status(book_title, new_status):
-    pass
+    for book in library:
+        if book["title"] == book_title:
+            book["isLoaned"] = not book["isLoaned"]
+            break
 
 
 def add_new_book(library, title, author, is_loaned):
-    pass
+    library.append({"title": title, "author": author, "isLoaned": is_loaned})
+    return library
 
 
 def remove_book(library, book_title):
-    pass
+    for book in library:
+        if book["title"] == book_title:
+            library.remove(book)
+            break
+    return library
 
 
 
